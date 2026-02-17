298922fc20217c6b67959414cd92f091cc1d777a
Davidandhisocs <148592182+Davidandhisocs@users.noreply.github.com>
2026-02-17 18:11:40 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -57,22 +57,23 @@ export async function fetchLeaderboard() {
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
-            completed: [],
-            progressed: [],
-        };
-        const { verified } = scoreMap[verifier];
-        verified.push({
-            rank: rank + 1,
-            level: level.name,
-            score: score(rank + 1, 100, level.percentToQualify),
-            link: level.verification,
-        });
+        // Verification (disabled – do not count verifiers on leaderboard)
+
+const verifier = Object.keys(scoreMap).find(
+    (u) => u.toLowerCase() === level.verifier.toLowerCase(),
+) || level.verifier;
+scoreMap[verifier] ??= {
+    verified: [],
+    completed: [],
+    progressed: [],
+};
+const { verified } = scoreMap[verifier];
+verified.push({
+    rank: rank + 1,
+    level: level.name,
+    score: score(rank + 1, 100, level.percentToQualify),
+    link: level.verification,
+});
 
         // Records
         level.records.forEach((record) => {
